19513077508903f7e45b3ab5bf48d9c8394e9ffb
Excalib88 <rave9373@gmail.com>
2018-02-09 23:16:55 +0300

    Commit #1
    Каркас бота(первые тестовые кнопки), обработка апдейтов и добавлен логгер

---
+++ CulinaryBot/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CulinaryBot
+{
+	static class Logger
+	{
+		public static void SendLogToFile(string exceptionText)
+		{
+			Console.WriteLine(exceptionText);
+		}
+	}
+}
--- CulinaryBot/Program.cs
+++ CulinaryBot/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace CulinaryBot
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		const string TOKEN = "484848836:AAGg2EXlGFJTVZC5BZPRJfdMnyGiK17ETrc";
+		static void Main(string[] args)
+		{
+
+			while (true)
+			{
+				try
+				{
+					GetMessages().Wait();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("GetMessage ERROR - " + ex);
+					Thread.Sleep(30000);
+				}
+			}
+		}
+		static async Task GetMessages()
+		{
+			TelegramBotClient bot = new TelegramBotClient(TOKEN);
+			int offset = 0;
+			int timeOut = 0;
+			try
+			{
+				await bot.SetWebhookAsync("");
+
+				while (true)
+				{
+					var updates = await bot.GetUpdatesAsync(offset, timeOut);
+					foreach (var update in updates)
+					{
+						var message = update.Message;
+						var keyboard = new Telegram.Bot.Types.ReplyMarkups.ReplyKeyboardMarkup
+						{
+							Keyboard = new[] {
+								new[]
+								{
+									new Telegram.Bot.Types.KeyboardButton("\U0001F4B0 Деньги \U0001F4B0"),
+									new Telegram.Bot.Types.KeyboardButton("\U0001F45B Донат \U0001F45B")
+								},
+								new[] // row 
+                                {
+									new Telegram.Bot.Types.KeyboardButton("Подписаться на рассылку"),
+									new Telegram.Bot.Types.KeyboardButton("Получить рецепт")
+								}
+							},
+							ResizeKeyboard = true
+						};
+
+						if (message.Text == "/start")
+						{
+							await bot.SendTextMessageAsync(message.Chat.Id, "<b>Приветствую " + message.Chat.FirstName + "</b>" +
+								"                                                                                    " + "Это кулинарный бот, который поможет тебе приготовить вкусности.",
+								Telegram.Bot.Types.Enums.ParseMode.Html, false, false, 0, keyboard);
+						}
+						offset = update.Id + 1;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Logger.SendLogToFile(ex.ToString());
+			}
+		}
+	}
 }
