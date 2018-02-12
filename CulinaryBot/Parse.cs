c622df67c4b5985f9729c27f5c4c38f8a2ee008f
Damir Mansurov <rave9373@gmail.com>
2018-02-12 21:43:20 +0300

    EditParseClass
    Add switch for recieve query

--- CulinaryBot/Parse.cs
+++ CulinaryBot/Parse.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.IO;
 using System.Text;
 using AngleSharp.Parser.Html;
@@ -10,40 +11,65 @@ namespace CulinaryBot
 {
 	static class Parse
 	{
+		static public string baseUrl = "https://andychef.ru/";
+
+
 		public static String GetCode(string urlAddress)
 		{
 			string data = "";
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
-			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-			if (response.StatusCode == HttpStatusCode.OK)
-			{
-				Stream receiveStream = response.GetResponseStream();
-				StreamReader readStream = null;
-				if (response.CharacterSet == null)
-				{
-					readStream = new StreamReader(receiveStream);
-				}
-				else
-				{
-					readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
-				}
-				data = readStream.ReadToEnd();
-				response.Close();
-				readStream.Close();
-			}
+			WebClient wc = new WebClient();
+			data = wc.DownloadString(urlAddress);
+			Console.WriteLine(data);
+			FindLink(data);
 			return data;
 		}
 
-		public static void GetLink()
+		public static void GetLinkForRecipe(string catalog)
 		{
-			var parser = new HtmlParser();
-			var document = parser.Parse(GetCode("http://russianfood.com/"));
-
-			var blueListItemsCssSelector = document.QuerySelectorAll("a.detail");
-
-			foreach (var item in blueListItemsCssSelector)
-				Console.WriteLine(item.ToString());
+			switch (catalog)
+			{
+				case "Десерт":
+					GetCode(baseUrl + "deserts/");
+					break;
+				case "Закуски":
+					GetCode(baseUrl + "zakuski/");
+					break;
+				case "Мясо":
+					GetCode(baseUrl + "myaso/");
+					break;
+				case "Напитки":
+					GetCode(baseUrl + "napitki/");
+					break;
+				case "Паста":
+					GetCode(baseUrl + "pasta/");
+					break;
+				case "Салаты":
+					GetCode(baseUrl + "salat/");
+					break;
+				case "Соусы":
+					GetCode(baseUrl + "sous/");
+					break;
+				case "Супы":
+					GetCode(baseUrl + "soup/");
+					break;
+				default:
+					{
+						Logger.SendLogToFile("Введённый каталог не найден!");
+						break;
+					}
+			}
 		}
+		public static void FindLink(string page)
+		{
+			string pattern = @"\b(\d+\W?руб)";
+			Regex regex = new Regex(pattern);
 
+			// Достигаем того же результата что и в предыдущем примере, 
+			// используя метод Regex.Matches() возвращающий MatchCollection
+			foreach (Match match in regex.Matches(page))
+			{
+				Console.WriteLine(match.Groups[1].Value);
+			}
+		}
 	}
 }
--- CulinaryBot/Program.cs
+++ CulinaryBot/Program.cs
@@ -57,7 +57,7 @@ namespace CulinaryBot
 							await bot.SendTextMessageAsync(message.Chat.Id, "<b>Приветствую " + message.Chat.FirstName + "</b>" +
 								"                                                                                    " + "Это кулинарный бот, который поможет тебе приготовить вкусности.",
 								Telegram.Bot.Types.Enums.ParseMode.Html, false, false, 0, keyboard);
-							Parse.GetLink();
+							Parse.GetLinkForRecipe("Десерт");
 							Database db = new Database(message.Chat.Id.ToString(), message.Chat.Username);
 							db.CreateNewUser();
 						}
